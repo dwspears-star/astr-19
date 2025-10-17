6eb9b788ac9154ca9c3956ba7bfadbf948d1584f
dwspears-star <dwspears@ucsc.edu>
2025-10-16 17:44:36 -0700

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,10 @@
+def printFullname():
+	print('Dash Spears')
+	print('I love the movie ponyo. I think its a really good movie')
+	print('My favorite food is miso soup. I think miso soup is really really yummy')
+
+def main():
+	printFullname()
+
+if __name__ == "__main__":
+	main()
