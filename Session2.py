efcd313c9919b2c1325674fb897ba9788385a269
dwspears-star <dwspears@ucsc.edu>
2025-10-19 23:06:36 -0700

    Add files via upload

---
+++ Session2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+num1 = float(input())
+num2 = float(input())
+
+print(num1+num2)
+print(type(num1+num2))
+
+print(int(num1-num2))
+print(type(int(num1-num2)))
+
+print(num1 - int(num2))
+print(type(num1 - int(num2)))
